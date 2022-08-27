20924dc8427c099cef64b8731e796f795a0d4283
Gabriel F. K <112198308+Gabbe-636@users.noreply.github.com>
2022-08-27 13:17:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextE1: ".swiper-button-next",
+    prevE1: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewhee1: true,
+  keyboard: true,
+});
